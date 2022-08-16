a206c5749790b5dd96001d72e0335afe1309e905
Jami Couch <jami.couch@twinsunsolutions.com>
2022-08-16 13:16:51 -0500

    [fit-and-finish-talk] Update api error and response to handle details

--- lib/model/api_error.dart
+++ lib/model/api_error.dart
@@ -1,8 +1,9 @@
 class ApiError {
   final int statusCode;
   final String message;
+  final Map<String, dynamic>? details;
 
-  ApiError(this.statusCode, this.message);
+  ApiError(this.statusCode, this.message, {this.details});
 
   factory ApiError.fromJson(Map json, int statusCode) {
     try {
@@ -10,6 +11,10 @@ class ApiError {
         var errorJson = json['errors'] is List ? (json['errors'] as List).first : json['errors'];
         if (errorJson is String) {
           return ApiError(statusCode, errorJson);
+        }
+
+        if (errorJson is Map<String, dynamic>) {
+          return ApiError(statusCode, 'Error with Details', details: errorJson);
         }
 
         return ApiError(statusCode, 'Unknown Error');
@@ -25,5 +30,10 @@ class ApiError {
     } catch (err) {
       return ApiError(statusCode, err.toString());
     }
+  }
+
+  @override
+  String toString() {
+    return 'ApiError(statusCode: $statusCode, message: $message, details: $details)';
   }
 }
