cecb1451cbb5a8944c0cedd4778a49869a4d05a4
Patrick Billingsley <patrick.billingsley@twinsunsolutions.com>
2024-06-19 09:05:47 -0500

    Updates Api
    - Wraps API components in base class

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,18 +1,23 @@
 import 'dart:io';
 
+import 'package:flutter_app_base/api/http_client.dart';
 import 'package:flutter_app_base/bloc/config_bloc.dart';
 
-const String apiUrl = 'http://localhost:3000';
+base class Api {
+  final String apiUrl = 'http://localhost:3000';
 
-Future<String> getAuthHeader() async {
-  String email = await ConfigBloc().streamFor(ConfigBloc.kAuthEmail).first;
-  String token = await ConfigBloc().streamFor(ConfigBloc.kAuthToken).first;
-  return 'Token token=$token,email=$email';
-}
+  HttpClient get client => HttpClient();
 
-Future<Map<String, String>> getDefaultHeaders() async {
-  return {
-    HttpHeaders.contentTypeHeader: 'application/json',
-    HttpHeaders.authorizationHeader: await getAuthHeader(),
-  };
+  Future<String> getAuthHeader() async {
+    final email = await ConfigBloc().streamFor(ConfigBloc.kAuthEmail).first;
+    final token = await ConfigBloc().streamFor(ConfigBloc.kAuthToken).first;
+    return 'Token token=$token,email=$email';
+  }
+
+  Future<Map<String, String>> getDefaultHeaders() async {
+    return {
+      HttpHeaders.contentTypeHeader: 'application/json',
+      HttpHeaders.authorizationHeader: await getAuthHeader(),
+    };
+  }
 }
