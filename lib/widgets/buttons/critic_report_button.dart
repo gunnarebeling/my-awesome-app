bc44ac87cd57d9eaf7cdeefea7f7bdc3f3558dd2
Patrick Billingsley <patrick.billingsley@twinsunsolutions.com>
2024-11-15 18:20:50 -0600

    Adds CriticReportButton

---
+++ lib/widgets/buttons/critic_report_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_base/widgets/critic_report_dialog.dart';
+
+class CriticReportButton extends StatelessWidget {
+  const CriticReportButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.help_outline),
+      onPressed: () {
+        showDialog(
+          context: context,
+          builder: (_) => const CriticReportDialog(),
+        );
+      },
+    );
+  }
+}
