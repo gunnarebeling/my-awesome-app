19fea734d682738ce5eeeb95b4b7e8ecf6c1b9f4
Patrick Billingsley <patrick.billingsley@twinsunsolutions.com>
2024-11-15 18:20:38 -0600

    Adds DefaultAppBar

---
+++ lib/widgets/app_bars/default_app_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_base/widgets/buttons/critic_report_button.dart';
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool centerTitle;
+  final List<Widget> actions;
+
+  const DefaultAppBar({
+    super.key,
+    this.title = '',
+    this.centerTitle = true,
+    this.actions = const [CriticReportButton()],
+  });
+
+  static const double appBarHeight = 60;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(appBarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 1,
+      shadowColor: Colors.black,
+      surfaceTintColor: Colors.transparent,
+      centerTitle: centerTitle,
+      title: Text(title),
+      actions: actions,
+    );
+  }
+}
