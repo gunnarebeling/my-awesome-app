0692b79eb1ebd3a20d1ae69d5a8333b54b489383
Patrick Billingsley <patrick.billingsley@twinsunsolutions.com>
2024-11-15 22:26:52 -0600

    Adds documentation to Navigation mixin

--- lib/mixins/navigation.dart
+++ lib/mixins/navigation.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 mixin Navigation<T extends StatefulWidget> on State<T> {
+  /// Pushes the given widget onto the navigation stack.
+  ///
+  /// {@macro flutter.widgets.navigator.pushNamed.returnValue}
+  ///
+  /// {@tool snippet}
+  ///
+  /// Typical usage is as follows:
+  ///
+  /// ```dart
+  /// void _openPage() {
+  ///   pushScreen(const MyScreen());
+  /// }
+  /// ```
+  /// {@end-tool}
   Future<E?> pushScreen<E extends Object?>(Widget screen) {
     return Navigator.of(context).push<E>(
       MaterialPageRoute(builder: (_) => screen),
     );
   }
 
+  /// Pushes the given widget onto the navigation stack and then removes all the previous widgets.
+  ///
+  /// {@macro flutter.widgets.navigator.pushNamed.returnValue}
+  ///
+  /// {@tool snippet}
+  ///
+  /// Typical usage is as follows:
+  ///
+  /// ```dart
+  /// void _resetAndOpenPage() {
+  ///   popAllAndPush(const MyScreen());
+  /// }
+  /// ```
+  /// {@end-tool}
   Future<E?> popAllAndPush<E extends Object?>(Widget screen) {
     return Navigator.of(context).pushAndRemoveUntil(
       MaterialPageRoute(builder: (_) => screen),
@@ -14,6 +42,30 @@ mixin Navigation<T extends StatefulWidget> on State<T> {
     );
   }
 
+  /// Pops the top-most route off the navigator.
+  ///
+  /// {@macro flutter.widgets.navigator.pop}
+  ///
+  /// {@tool snippet}
+  ///
+  /// Typical usage for closing a route is as follows:
+  ///
+  /// ```dart
+  /// void _handleClose() {
+  ///   popScreen();
+  /// }
+  /// ```
+  /// {@end-tool}
+  /// {@tool snippet}
+  ///
+  /// A dialog box might be closed with a result:
+  ///
+  /// ```dart
+  /// void _handleAccept() {
+  ///   popScreen(true); // dialog returns true
+  /// }
+  /// ```
+  /// {@end-tool}
   void popScreen<E extends Object?>([E? result]) {
     return Navigator.of(context).pop<E>(result);
   }
