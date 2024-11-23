2ea7066fe77994b5e9b398651abb0a0b2f519b06
Patrick Billingsley <patrick.billingsley@twinsunsolutions.com>
2024-11-22 18:03:49 -0600

    Updates MainScreen to use BaseScreen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,56 +1,88 @@
+import 'dart:async';
+
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_base/bloc/login_bloc.dart';
+import 'package:flutter_app_base/mixins/stateless_navigation.dart';
 import 'package:flutter_app_base/model/user.dart';
+import 'package:flutter_app_base/screens/base_screen.dart';
 import 'package:flutter_app_base/screens/login_screen.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
+class MainScreen extends BaseScreen {
+  const MainScreen({
+    super.key,
+    super.title = 'Main Screen',
+  });
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends BaseScreenState<MainScreen> {
+  late final StreamSubscription<User?> _currentUserSubscription;
+
+  User? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentUserSubscription = LoginBloc().currentUser.listen(_onCurrentUserChanged);
+  }
+
+  @override
+  void dispose() {
+    _currentUserSubscription.cancel();
+    super.dispose();
+  }
+
+  void _onCurrentUserChanged(User? user) {
+    setState(() {
+      _currentUser = user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context, [_]) {
+    return super.build(
+      context,
+      SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_currentUser?.email ?? 'No user'),
+            const _LogoutButton(),
+            TextButton(
+              onPressed: FirebaseCrashlytics.instance.crash,
+              child: const Text('Crash'),
+            ),
+            TextButton(
+              onPressed: () =>  FirebaseCrashlytics.instance.recordError('Test Report', null),
+              child: const Text('Non-fatal Firebase Report'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _LogoutButton extends StatelessWidget with StatelessNavigation {
+  const _LogoutButton();
+
+  Future<void> _onPressed(BuildContext context) async {
+    await LoginBloc().logout();
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => popAllAndPush(context, const LoginScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<User?>(
-        stream: LoginBloc().currentUser,
-        builder: (context, snapshot) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Center(
-                child: Text('Main Screen'),
-              ),
-              Center(
-                child: Text(snapshot.data?.email ?? 'No user'),
-              ),
-              TextButton(
-                onPressed: () async {
-                  await LoginBloc().logout();
-                  Future.delayed(const Duration(seconds: 2), () {
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                  });
-                },
-                child: const Text('Logout'),
-              ),
-              TextButton(
-                onPressed: () {
-                  FirebaseCrashlytics.instance.crash();
-                },
-                child: const Text('Crash'),
-              ),
-              TextButton(
-                onPressed: () {
-                  FirebaseCrashlytics.instance.recordError('Test Report', null);
-                },
-                child: const Text('Non-fatal Firebase Report'),
-              ),
-            ],
-          );
-        },
-      ),
+    return TextButton(
+      onPressed: () => _onPressed(context),
+      child: const Text('Logout'),
     );
   }
 }
